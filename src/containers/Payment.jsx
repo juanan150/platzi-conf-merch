795a974ddd94635db0ec01e75f6d8d5498633f0f
Juan <juanan0309@gmail.com>
2021-08-09 20:57:36 -0500

    Added Google Maps and Paypal API integrations

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Payment.css';
 
-export default function Payment() {
+export default function Payment({ history }) {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+
+  const paypalOptions = {
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'react',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        products: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+    }
+    history.push('/checkout/success');
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido</h3>
-        <div className="Payment-button">Boton de pago con Paypal</div>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onStart={() => console.log('Start Payment')}
+            onSuccess={(data) => handlePaymentSuccess(data)}
+            onError={(error) => console.log(error)}
+            onCancel={(data) => console.log(data)}
+          />
+        </div>
       </div>
       <div></div>
     </div>
---
+++ src/hooks/useGoogleAddress.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useGoogleAddress = (address) => {
+  const [map, setMap] = useState({});
+  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.RACT_APP_MAP_KEY}`;
+
+  useEffect(async () => {
+    const response = await axios(API);
+    setMap(response.data.results[0].geometry.location);
+  }, []);
+
+  return map;
+};
+
+export default useGoogleAddress;
